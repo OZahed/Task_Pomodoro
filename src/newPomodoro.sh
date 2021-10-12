79fa2aa51f28afb3945a9fb77c20e72d698f9ac0
Omid Zahed <omid.zahed.re@gmail.com>
2021-10-12 11:57:27 -0700

    Poodoro added

--- src/newPomodoro.sh
+++ src/newPomodoro.sh
@@ -1,5 +1,45 @@
 #! /bin/bash
 
-echo "this is the Pmodoro.sh"
-echo $#
-echo $@
+unset NAME
+unset MIN
+
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+
+MIN=25
+PARAMS=""
+while (( "$#" )); do
+case "$1" in 
+-t|--time)
+    MIN="$2"
+    shift
+    ;;
+-n|--name)
+    NAME="$2"
+    shift
+    ;;
+*)  
+    PARAMS="$PARAMS $1"
+    shift
+    ;;
+esac 
+done
+
+if [[ -z "$NAME" ]]
+then
+    echo "Title can't be empty"
+    exit 1
+fi 
+
+SECOND=$((60 * MIN))
+printf "\n\n"
+while [  $SECOND -gt 0 ]
+do
+    MIN=$(( SECOND / 60 ))
+    SEC=$(( SECOND - MIN*60 ))
+    printf "\r\t$NAME: ${ORANGE}$MIN : $SEC ${NC}."  $((SECOND--))
+    sleep 1s
+done
+
